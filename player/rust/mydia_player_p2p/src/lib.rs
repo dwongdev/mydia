f9c89aaec50d301bb5fd326cbbc20c0c1722c5e5
Alexandre Rosenfeld <arsfeld@gmail.com>
2026-01-11 20:24:37 -0500

    feat(p2p): expose event stream to Flutter via FRB

--- player/rust/mydia_player_p2p/src/lib.rs
+++ player/rust/mydia_player_p2p/src/lib.rs
@@ -1,5 +1,6 @@
-use mydia_p2p_core::Host;
+use mydia_p2p_core::{Host, Event};
 use flutter_rust_bridge::frb;
+use flutter_rust_bridge::StreamSink;
 
 #[frb(init)]
 pub fn init_app() {
@@ -20,5 +21,27 @@ impl P2pHost {
 
     pub fn listen(&self, addr: String) -> anyhow::Result<()> {
         self.inner.listen(addr).map_err(|e| anyhow::anyhow!(e))
+    }
+
+    pub fn dial(&self, addr: String) -> anyhow::Result<()> {
+        self.inner.dial(addr).map_err(|e| anyhow::anyhow!(e))
+    }
+
+    pub fn event_stream(&self, sink: StreamSink<String>) -> anyhow::Result<()> {
+        let rx = self.inner.event_rx.clone();
+        tokio::spawn(async move {
+            let mut rx = rx.lock().await;
+            while let Some(event) = rx.recv().await {
+                let msg = match event {
+                    Event::PeerDiscovered(id) => format!("peer_discovered:{}", id),
+                    Event::PeerExpired(id) => format!("peer_expired:{}", id),
+                    _ => "unknown".to_string(),
+                };
+                if sink.add(msg).is_err() {
+                    break;
+                }
+            }
+        });
+        Ok(())
     }
 }
